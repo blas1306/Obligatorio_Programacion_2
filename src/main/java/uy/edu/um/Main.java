861cee4f8cedb0562ac6bb51dc4d1ca740dec263
vikycota <vcota@correo.um.edu.uy>
2026-05-28 09:38:27 -0300

    Help

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -2,7 +2,7 @@ package uy.edu.um;
 import uy.edu.um.doors.ProcessConsole;
 import uy.edu.um.doors.ProcessManagerImpl;
 
-//Viky Test
+//Viky no puede mas con su vida
 //Manu Test
 public class Main {
     public static void main(String[] args) {
