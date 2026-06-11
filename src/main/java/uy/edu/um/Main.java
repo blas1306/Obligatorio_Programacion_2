70caa2fb8007cf6211326a8cfe1396baaf61c244
blasm <blasmelissaric@gmail.com>
2026-06-11 09:00:42 -0300

    borrando aun mas comentarios innecesarios

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -2,9 +2,6 @@ package uy.edu.um;
 import uy.edu.um.doors.ProcessConsole;
 import uy.edu.um.doors.ProcessManagerImpl;
 
-//Viky no puede mas con su vida, matenme
-//Manu Test
-//Seba Test, HOLAAA
 public class Main {
     public static void main(String[] args) {
 
